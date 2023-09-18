18516dbec99fb56b34d206ba6592661f3241c220
chanda chewe <chewec03@gmail.com>
2023-09-18 23:18:48 +0200

    Added PHP Code Style Formatter styleci

--- tests/Unit/EnvTest.php
+++ tests/Unit/EnvTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once './vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
 
@@ -6,8 +6,7 @@ $dotenv->safeLoad();
 
 it('checks DotEnv', function () {
     $MONEYUNIFY_UUID = $_ENV['MONEYUNIFY_UUID'];
-  
+
 
     expect($MONEYUNIFY_UUID)->toEqual(3);
-    
- });
+});
