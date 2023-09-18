38a37d76893b55807f23e24ba9daf7e846a92201
chanda chewe <chewec03@gmail.com>
2023-09-18 23:06:47 +0200

    Added pests test for .env

---
+++ tests/Unit/EnvTest.php
@@ -0,0 +1,13 @@
+<?php 
+require_once './vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+
+$dotenv->safeLoad();
+
+it('checks DotEnv', function () {
+    $MONEYUNIFY_UUID = $_ENV['MONEYUNIFY_UUID'];
+  
+
+    expect($MONEYUNIFY_UUID)->toEqual(3);
+    
+ });
